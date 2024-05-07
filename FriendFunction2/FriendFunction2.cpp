f106415f7198598a2281ac829a64bad244e1853c
Mihaly24 <fairuzk20@gmail.com>
2024-05-07 18:51:24 +0700

    Menyelesaikan projek friend function dua

--- FriendFunction2/FriendFunction2.cpp
+++ FriendFunction2/FriendFunction2.cpp
@@ -2,7 +2,32 @@
 #include <string>
 using namespace std;
 
+class pelajar;
+class manusia
+{
+public:
+	void showNilaiPelajar(pelajar& ax);
+};
+
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x)
+{
+	cout << x.nilai;
+}
+
 int main()
 {
-
+	manusia budi;
+	pelajar pbudi;
+	budi.showNilaiPelajar(pbudi);
+	return 0;
 }
